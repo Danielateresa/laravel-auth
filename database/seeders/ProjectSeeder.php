43c0b416045d681cc4a59383a8d2dd133461d23e
Danielateresa <dany.t_87@hotmail.it>
2023-01-10 16:02:25 +0100

    Added Project migration and seeder

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -4,6 +4,10 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faher\Generator as Faker;
+
+use Illuminate\Support\Str;
+use App\Models\Project;
 
 class ProjectSeeder extends Seeder
 {
@@ -12,8 +16,14 @@ class ProjectSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i <10 ; $i++) {
+            $project = new project();
+            $project->title = $faker->sentence(3);
+            $project->slug = str::slug($project->$title, '-'); //lo slug viene creato dal title sostituendo gli spazi con -
+            $project->description = $faker->text();
+            $project->save();
+        }
     }
 }
